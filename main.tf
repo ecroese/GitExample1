925112e2b8eb3b50ea42b2aef8dfd914c398260d
ecroese <125081882+ecroese@users.noreply.github.com>
2024-01-22 10:57:10 +0100

    another commit

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "Fontys_NL"
-  location = var.location
+ ccc==dd
 }
 resource "azurerm_virtual_network" "vnet2" {
-  name                = "Vlan-88"
+  name                = "Vlan-99"
   address_space       = ["192.168.88.0/24"]
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-}
+  resource_group_name = resourceg.name
+  }
